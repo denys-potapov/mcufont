ef83f26595e289a343c055d865bd85dd8c84e4b9
Denys Potapov <denys.potapov@gmail.com>
2019-07-09 11:22:24 +0300

    testing tool

---
+++ examples/render_bmp/test_all.sh
@@ -0,0 +1,14 @@
+cd ../../
+cd encoder
+make
+
+cd ../fonts
+../encoder/mcufont import_ttf DejaVuSans.ttf 12
+../encoder/mcufont filter DejaVuSans12.dat 0-255 0x2010-0x2015
+../encoder/mcufont rlefont_export DejaVuSans12.dat
+
+cd ../examples/render_bmp
+make -B
+
+# ./render_bmp -a l -f DejaVuSerif16
+./render_bmp -a l -f DejaVuSans12
