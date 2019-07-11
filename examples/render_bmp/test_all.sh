e5937097461db8f0c465f559dc3a641d5164854c
Denys Potapov <denys.potapov@gmail.com>
2019-07-11 17:57:13 +0300

    export path

--- examples/render_bmp/test_all.sh
+++ examples/render_bmp/test_all.sh
@@ -2,6 +2,9 @@ cd ../../
 cd encoder
 make
 
+# LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH ./mcufont 
+export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
+
 cd ../fonts
 ../encoder/mcufont import_ttf DejaVuSans.ttf 12
 ../encoder/mcufont filter DejaVuSans12.dat 0-255 0x2010-0x2015
