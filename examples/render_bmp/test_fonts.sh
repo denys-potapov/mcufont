f2c760e044eeff613621f229c401c3781ba4b0e9
Denys Potapov <denys.potapov@gmail.com>
2019-08-12 14:53:37 +0300

    add PT mono to test set

--- examples/render_bmp/test_fonts.sh
+++ examples/render_bmp/test_fonts.sh
@@ -18,6 +18,9 @@ cd ../fonts
 ../encoder/mcufont filter DejaVuSansMono12.dat 0-255 0x2010-0x2015
 ../encoder/mcufont rlefont_export DejaVuSansMono12.dat
 
+../encoder/mcufont import_ttf PT_mono.ttf 12
+../encoder/mcufont filter PT_mono12.dat 0-255 0x2010-0x2015
+../encoder/mcufont rlefont_export PT_mono12.dat
 make fonts.h
 
 cd ../examples/render_bmp
@@ -27,3 +30,4 @@ make -B
 ./render_bmp -a l -w 120 -o out_12.bmp -f DejaVuSans12
 ./render_bmp -a l -w 120 -o out_14.bmp -f DejaVuSans14
 ./render_bmp -a l -w 120 -o out_mono.bmp -f DejaVuSansMono12
+./render_bmp -a l -w 120 -o out_pt.bmp -f PT_mono12
